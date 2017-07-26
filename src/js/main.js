91f2de8f53acf446afaa7db600fb351674e2e842
Dmitry Bruslik <dmitry.bruslik@yandex.ua>
2017-07-26 15:18:26 +0300

    Добавил скрипт закрытия и открытия меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 var burger = document.querySelector('.burger');
+var menu = document.querySelector('.about__menu');
 burger.addEventListener('click', function(){
-	var classList = burger.getAttribute('class');
-	if ( /open/.test(classList) ) {
-		burger.setAttribute('class');
+	burger.classList.toggle('open');
+	if ( /open/.test(burger.getAttribute('class')) ) {
+		menu.classList.add('open');
 	}
+	else menu.classList.remove('open'); 
 });
